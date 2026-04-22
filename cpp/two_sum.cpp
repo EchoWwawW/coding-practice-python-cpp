967ce31abbeb182d58bbd8015f7ea26e18a39b4f
EchoWwawW <zimengwan77@gmail.com>
2026-04-22 12:44:26 +0800

    Update two_sum.cpp

--- cpp/two_sum.cpp
+++ cpp/two_sum.cpp
@@ -1,1 +1,19 @@
 
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (hashmap.count(diff)) {
+                return {hashmap[diff], i};
+            }
+            hashmap[nums[i]] = i;
+        }
+        return {};
+    }
+};
